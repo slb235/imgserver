da76aea7b647a9517c7e7e739976cf708bdd4f8b
Jonathan Rauprich <rauprich.jonathan@googlemail.com>
2017-05-09 19:51:54 +0200

    gif support

--- index.js
+++ index.js
@@ -5,8 +5,9 @@ const async = require('async')
 const sharp = require('sharp')
 const compile = require('string-template/compile')
 const gs = require('@google-cloud/storage')
+const gm = require('gm').subClass({ imageMagick: true })
 
-const config = require('../config')
+const config = require('../../config')
 
 const app = express()
 
@@ -65,65 +66,97 @@ Object.keys(config.endPoints).forEach((endPointKey) => {
         next(new Error('File not found'))
         return
       }
-      let op = sharp()
-      endPoint.operations.forEach((operation) => {
-        switch(operation.type) {
-          case 'rotate':
-            op = op.rotate()
-            break
-          case 'resize':
-            op = op.resize(operation.size[0], operation.size[1])
-            break
-          case 'crop':
-            switch(operation.strategy) {
-              case 'attention':
-                op = op.crop(sharp.strategy.attention)
-                break
-              default:
-                op = op.crop(sharp.gravity.centre)
-            }
-            break
-          case 'blur':
-            op = op.blur(20)
-            break
-          case 'overlay':
-            op = op.overlayWith(new Buffer(operation.image), { gravity: sharp.gravity.center })
-            break
-          case 'negate':
-            op = op.negate()
-            break
-          default:
-            throw new Error('Unsupportet operation: ' + operation.type)
-        }
-      })
-      let contentType
-      switch(req.params.format) {
-        case 'jpg':
-        case 'jpeg':
-          op = op.jpeg(endPoint.outputOptions && endPoint.outputOptions.jpeg ? endPoint.outputOptions.jpeg : config.outputOptions.jpeg)
-          contentType = 'image/jpeg'
-          break;
-        case 'png':
-          op = op.png(endPoint.outputOptions && endPoint.outputOptions.png ? endPoint.outputOptions.png : config.outputOptions.png)
-          contentType = 'image/png'
-          break;
-        case 'webp':
-          op = op.webp(endPoint.outputOptions && endPoint.outputOptions.webp ? endPoint.outputOptions.webp : config.outputOptions.webp)
-          contentType = 'image/webp'
-          break;
-        default:
-          next('uknown format')
+      if(req.params.format == 'gif') {
+        // for gifs we have to go the gm route (to support animated gifs)
+        let magick = gm(imageStream).noProfile()
+        let size = [null, null]
+        endPoint.operations.forEach((operation) => {
+          switch(operation.type) {
+            case 'rotate':
+              magick = magick.autoOrient()
+              break
+            case 'resize':
+              size = operation.size;
+              magick = magick.resize(operation.size[0], operation.size[1], '^')
+              break
+            case 'crop':
+              magick = magick.crop(size[0], size[1])
+              break
+          }
+        })
+        res.set('Content-Type', 'image/gif')
+        magick.stream((err, stdout, stderr) => {
+          if(err) {
+            next(err)
+            return
+          }
+          stdout.pipe(res)
+          stderr.on('error', next)
+        })
       }
-      op.on('error', (err) => {
-        next(err)
-      })
-      res.set('Content-Type', contentType)
-      imageStream.on('error', (err) => {
-        next(err)
-      })
-      imageStream.pipe(op).pipe(res)
+      else {
+        let op = sharp()
+        endPoint.operations.forEach((operation) => {
+          switch(operation.type) {
+            case 'rotate':
+              op = op.rotate()
+              break
+            case 'resize':
+              op = op.resize(operation.size[0], operation.size[1])
+              break
+            case 'crop':
+              switch(operation.strategy) {
+                case 'attention':
+                  op = op.crop(sharp.strategy.attention)
+                  break
+                default:
+                  op = op.crop(sharp.gravity.centre)
+              }
+              break
+            case 'blur':
+              op = op.blur(20)
+              break
+            case 'overlay':
+              op = op.overlayWith(new Buffer(operation.image), { gravity: sharp.gravity.center })
+              break
+            case 'negate':
+              op = op.negate()
+              break
+            default:
+              throw new Error('Unsupportet operation: ' + operation.type)
+          }
+        })
+        let contentType
+        switch(req.params.format) {
+          case 'png':
+            op = op.png(endPoint.outputOptions && endPoint.outputOptions.png ? endPoint.outputOptions.png : config.outputOptions.png)
+            contentType = 'image/png'
+            break
+          case 'webp':
+            op = op.webp(endPoint.outputOptions && endPoint.outputOptions.webp ? endPoint.outputOptions.webp : config.outputOptions.webp)
+            contentType = 'image/webp'
+            break
+          case 'jpg':
+          case 'jpeg':
+          default:
+            op = op.jpeg(endPoint.outputOptions && endPoint.outputOptions.jpeg ? endPoint.outputOptions.jpeg : config.outputOptions.jpeg)
+            contentType = 'image/jpeg'
+        }
+        op.on('error', (err) => {
+          next(err)
+        })
+        res.set('Content-Type', contentType)
+        imageStream.on('error', (err) => {
+          next(err)
+        })
+        imageStream.pipe(op).pipe(res)
+      }
     })
   })
+})
+
+app.use((err, req, res, next) => {
+  res.status(404).send('File not found')
 })
 
 // pm2 downtime free support support

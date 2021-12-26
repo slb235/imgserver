e114045db01399e486131e0650c6b64f3232ab8d
Jonathan Rauprich <rauprich.jonathan@googlemail.com>
2021-12-26 22:25:37 +0100

    gifsicle gif instead of imagemagick, paramcrop support, cleanup

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 "use strict";
-const fs = require('fs')
 const express = require('express')
-const async = require('async')
 const sharp = require('sharp')
-const compile = require('string-template/compile')
-const gm = require('gm').subClass({ imageMagick: true })
+const gifResize = require('actionbound-gif-resize-stream')
 const ActionboundStorageClient = require('actionbound-storage-client')
+const sizeOf = require('image-size')
 
 const config = require('./config')
-
 
 const storage = new ActionboundStorageClient(config.storage)
 
 const app = express()
+
+async function getStreamImageSize(stream) {
+	const chunks = []
+	for await (const chunk of stream) {
+		chunks.push(chunk)
+		try {
+			return sizeOf(Buffer.concat(chunks));
+		} catch (error) {/* Not ready yet */}
+	}
+
+	return sizeOf(Buffer.concat(chunks));
+}
 
 app.get('/health', (req, res) => {
   res.end('good')
@@ -20,12 +29,6 @@ app.get('/health', (req, res) => {
 
 Object.keys(config.endPoints).forEach((endPointKey) => {
   const endPoint = config.endPoints[endPointKey]
-  /*
-  const sourceset = config.sourceSets[endPoint.sourceSet].map((source) => (
-    Object.assign(source, { _target: compile(source.target) })
-  ))
-  */
-
   const target = config.sourceSets[endPoint.sourceSet]
 
   app.get(endPointKey, (req, res, next) => {
@@ -34,75 +37,94 @@ Object.keys(config.endPoints).forEach((endPointKey) => {
       file = file.replace(`{${key}}`, req.params[key])      
     }
     storage.createReadStream(file)
-    .then((imageStream) => {
+    .then(async (imageStream) => {
       if(req.params.format == 'gif') {
-        // for gifs we have to go the gm route (to support animated gifs)
-        gm(imageStream).size((err, originalSize) => {
-          if(err) {
-            next(err)
-            return
-          }
-          storage.createReadStream(file)
-          .then((imageStream) => {
-            let magick = gm(imageStream).noProfile()
-            let size = [null, null], needResize = false
-            endPoint.operations.forEach((operation) => {
-              switch(operation.type) {
-                case 'rotate':
-                  magick = magick.autoOrient()
-                  break
-                case 'resize':
-                  size = operation.size
-                  needResize = true
-                  break
-                case 'crop':
-                  needResize = false
-                  let targetRatio = size[0] / size[1]
-                  let originalRatio = originalSize.width / originalSize.height
-                  let cropX, cropY, cropWidth, cropHeight
-                  if(originalRatio > targetRatio) {
-                    cropWidth = originalSize.height * targetRatio
-                    cropHeight = originalSize.height
-                    cropX = (originalSize.width - cropWidth) / 2
+        
+        let srcSize = null
+        try {
+          const sizeStream = await storage.createReadStream(file)
+          srcSize = await getStreamImageSize(sizeStream)  
+        }
+        catch (err) {
+          // need to work without size
+        }
+
+        const transform = {  }
+
+        endPoint.operations.forEach((operation) => {
+          // gif operations are handled a bit differntly
+          // no rotation (exif), since i dont think it's a problem
+          // no crop, this is handled when width and height is specified
+          
+          switch(operation.type) {
+            case 'resize':
+              // ignore opreation crop, since it is used, when width and height are set
+              const [targetWidth, targetHeight] = operation.size
+              if(targetWidth && targetHeight) {
+                if(srcSize) {
+                  const targetRatio = targetWidth / targetHeight
+                  const srcRatio = srcSize.width / srcSize.height
+
+                  let cropX, cropY, cropWidth, cropHeight, scale
+
+                  if(srcRatio > targetRatio) {
+                    cropWidth = srcSize.height * targetRatio
+                    cropHeight = srcSize.height
+                    cropX = (srcSize.width - cropWidth) / 2
                     cropY = 0
+                    scale = targetWidth/cropWidth
                   }
                   else {
-                    if(originalRatio < targetRatio) {
-                      cropWidth = originalSize.width
-                      cropHeight = originalSize.width / targetRatio
-                      cropX = 0
-                      cropY = (originalSize.height - cropHeight) / 2
-                    }
-                    else {
-                      cropWidth = 0
-                      cropHeight = 0
-                      cropX = 0
-                      cropY = 0
-                    }
+                    cropWidth = srcSize.width
+                    cropHeight = srcSize.width / targetRatio
+                    cropX = 0
+                    cropY = (srcSize.height - cropHeight) / 2
+                    scale = targetHeight/cropHeight
                   }
-                  magick = magick.crop(cropWidth, cropHeight, cropX, cropY).resize(size[0], size[1]).out('+repage')
-                  break
+                  
+                  transform.crop = [cropX,cropY,cropWidth,cropHeight].map(Math.floor)
+                  transform.scale = scale
+                }
+                else {
+                  // fallback, just use whatever fits best
+                  transform.width = targetWidth
+                  transform.height = targetHeight
+                }
               }
-            })
-            if(needResize) {
-              if((originalSize.width > size[0] && size[0]) || (originalSize.height > size[1] && size[1])) {
-                magick = magick.resize(size[0], size[1])
+              else {
+                if(targetWidth) {
+                  if(srcSize && !operation.withoutEnlargement && targetWidth > srcSize.width) {
+                    // upscale
+                    transform.scale = (targetWidth/srcSize.width)
+                  }
+                  else {
+                    transform.width = targetWidth
+                  }
+                }
+                if(targetHeight) {
+                  if(srcSize && !operation.withoutEnlargement && targetHeight > srcSize.height) {
+                    transform.scale = (targetHeight/srcSize.height)
+                  }
+                  else {
+                    transform.height = targetHeight
+                  }
+                }
               }
-            }
-            res.set('Content-Type', 'image/gif')
-            magick.stream((err, stdout, stderr) => {
-              if(err) {
-                next(err)
-                return
-              }
-              stdout.pipe(res)
-              stderr.on('error', next)
-            })
-          })
-          .catch((err) => {
-            next(err)
-          })
+              break
+            case 'paramcrop':
+              transform.crop = [Number(req.params.left), Number(req.params.top), Number(req.params.width), Number(req.params.height)].map(Math.floor)
+              break
+          }
         })
+
+        const gif = gifResize(transform)(imageStream)
+
+        res.set('Content-Type', 'image/gif')
+        
+        gif.stdout.pipe(res)
+        gif.stderr.resume()
+
+        gif.then(() => res.end()).catch(() => res.end())
       }
       else {
         let op = sharp()
@@ -180,7 +202,7 @@ Object.keys(config.endPoints).forEach((endPointKey) => {
       }
     })
     .catch((error) => {
-      console.log(error)
+      console.log(error.message)
       next(new Error('File not found'))
       return
     })
